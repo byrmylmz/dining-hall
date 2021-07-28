9e3154a526d6041af41ccc907aa523d57a3fc630
byrmylmz <68708205+byrmylmz@users.noreply.github.com>
2021-07-28 12:26:06 +0300

    css optimization

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
 
         @livewireStyles
 
