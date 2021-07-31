34b42bed180204ec42f868df77eb9ede19919275
byrmylmz <68708205+byrmylmz@users.noreply.github.com>
2021-07-31 16:51:21 +0300

    double

--- app/Http/Controllers/dahsboardController.php
+++ app/Http/Controllers/dahsboardController.php
@@ -9,9 +9,11 @@ use Illuminate\Support\Facades\DB;
 class dahsboardController extends Controller
 {
     public function dashboard(){
+
         $totalPerson=DB::table('people')->count();
         $totalCompany=DB::table('companies')->count();
-        $totalBalance=DB::table('people')->select( DB::raw('SUM(balance) as balance '))->first();
+        $totalBalance=DB::table('people')->select( DB::raw('SUM(cast(balance as double precision)) as balance '))->first();
+
         return view('dashboard',compact('totalPerson','totalCompany','totalBalance'));    
     }
 }
